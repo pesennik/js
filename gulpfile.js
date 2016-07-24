bac5a88eeaf939edc524b1ae8a54f10be6e4afee
Mikhail Fursov <fursovm@amazon.com>
2016-07-23 20:51:05 -0700

    Использование site.min.js напрямую из js модуля

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,22 @@ var source = require("vinyl-source-stream");
 var browserify = require("browserify");
 var tsify = require("tsify");
 var shim = require("browserify-shim");
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task("build", function () {
-    return browserify(["./src/main.ts", "./typings/tsd.d.ts"])
+    browserify(["./src/main.ts", "./typings/tsd.d.ts"])
         .plugin(tsify)
         .transform(shim)
         .bundle()
         .pipe(source("site.js"))
-        .pipe(gulp.dest("./package/js/"))
+        .pipe(gulp.dest("./package/"));
+
+    gulp.src("./package/site.js")
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(uglify())
+        .pipe(gulp.dest("./package/"));
 });
 
-
-gulp.task("build-tests", function () {
-    return browserify(["./spec/tests.ts", "./typings/tsd.d.ts"])
-        .plugin(tsify)
-        .transform(shim)
-        .bundle()
-        .pipe(source("tests.js"))
-        .pipe(gulp.dest("./package/js/"))
-});
-
-gulp.task("deploy-site-js", ["build"], function () {
-    return gulp.src("./package/js/site.js")
-        .pipe(gulp.dest("../pesennik/src/main/webapp/js/"));
-});
 
 gulp.task("default", ["build"]);
